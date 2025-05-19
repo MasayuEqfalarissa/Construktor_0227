7651df543a005065a74d896a0a53d717ef06776a
MasayuEqfalarissa <masayueqfalarissaa@gmail.com>
2025-05-19 19:40:59 +0700

    void angka

--- destructor.cpp
+++ destructor.cpp
@@ -27,3 +27,15 @@ angka::~angka(){//Destructor
     cout<<"Alamat Array Sudah Dilrpaskan"<<endl;
 }
 
+void angka::cetakData(){
+    for(int i=1;i<=panjang;i++){
+        cout<<i<<" = "<<arr[i]<<endl;
+    }
+}
+
+
+  
+
+
+
+
