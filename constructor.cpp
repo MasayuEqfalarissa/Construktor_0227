89bab1baf752eac10b6b3533d8be1761bc5604b8
MasayuEqfalarissa <masayueqfalarissaa@gmail.com>
2025-05-19 19:44:00 +0700

    private class

--- constructor.cpp
+++ constructor.cpp
@@ -6,4 +6,12 @@ class mahasiswa{
 private :
     int nim;
     string nama;
+
+    public:
+    mahasiswa();
+    mahasiswa(int);
+    mahasiswa(string);
+    mahasiswa(int iNim, string iNama);
+    void cetak();
 };
+
